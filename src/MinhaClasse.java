5b344b205c45de98ebb7371706e9a47c21856893
Jose Bento <jldbento@gmail.com>
2024-04-24 07:10:55 -0300

    Atualização git add .!

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,12 +1,20 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        String nomeDeCarro = "Dodge";
-
+        String nomeDaMarcaDeCarro = "Dodge";
         int anoFabricação = 2024;
-
         boolean carro = true;
 
-        
+        String primeiroNome = "Joseph";
+        String segundoNome = "Stalyn";
+
+        String nomeCompleto = nomeDoFabricante(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
 
     }
+
+    public static String nomeDoFabricante (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+
 }
