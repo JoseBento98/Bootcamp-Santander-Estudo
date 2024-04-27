620e179876e590429b8f9e200a62cf30ea7b2a01
Jose Bento <jldbento@gmail.com>
2024-04-27 14:07:51 -0300

    Aula de Do While com metodo Auxxiliar

---
+++ src/jose/luiz/primeiraSemana/ExemploDoWhile.java
@@ -0,0 +1,23 @@
+package jose.luiz.primeirasemana;
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+        do{
+            //executando repetidas vezes até alguem atender
+            System.out.println("Telefone tocando");
+
+        }while(tocando());
+        System.out.println("Alô !!!");
+    }
+
+    private static boolean tocando() { // metodo auxiliar randomico
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+        //negando o ato de continuar tocando
+        return ! atendeu;
+    }
+
+
+}
