3d920a768ac6796cca7d05a3f572ab6dd359025b
Jose Bento <jldbento@gmail.com>
2024-05-05 11:01:28 -0300

    Aula Basica sobre pacotes e classes POO 8

--- src/jose/luiz/segundasemana/escola/Escola.java
+++ src/jose/luiz/segundasemana/escola/Escola.java
@@ -3,9 +3,10 @@ package jose.luiz.segundasemana.escola;
 public class Escola {
           public static void main(String[] args) {
                     Aluno felipe = new Aluno();
-                    felipe.nome = "Felipe";
-                    felipe.idade = 8;
+                    felipe.setNome("Felipe");
+                    felipe.setIdade(8);
 
-                    System.out.println("O aluno " + felipe.nome + " Tem: " + felipe.idade + " Anos de idade");
+                    System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " Anos de Idade");
+                    
           }
 }
