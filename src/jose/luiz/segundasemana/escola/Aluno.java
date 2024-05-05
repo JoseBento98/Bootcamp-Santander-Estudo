59a5ac1b02bb547f2fcbd4517910a1640adaf6d3
Jose Bento <jldbento@gmail.com>
2024-05-05 11:47:10 -0300

    Aula Basica sobre pacotes e classes POO 9

--- src/jose/luiz/segundasemana/escola/Aluno.java
+++ src/jose/luiz/segundasemana/escola/Aluno.java
@@ -4,6 +4,14 @@ public class Aluno {
           private String nome;
           private int idade;
 
+          private String sexo;
+
+          public String getSexo() {
+                    return sexo;
+          }
+          public void setSexo(String sexo) {
+                    this.sexo = sexo;
+          }
           public String getNome () {
                     return nome;
           }
--- src/jose/luiz/segundasemana/escola/Escola.java
+++ src/jose/luiz/segundasemana/escola/Escola.java
@@ -5,8 +5,9 @@ public class Escola {
                     Aluno felipe = new Aluno();
                     felipe.setNome("Felipe");
                     felipe.setIdade(8);
+                    felipe.setSexo("Mascolino");
 
-                    System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " Anos de Idade");
+                    System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " Anos de Idade e seu sexo:" + felipe.getSexo() );
                     
           }
 }
