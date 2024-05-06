3fa4b48b543b0b45bfda85eb955e0f07b1c74aee
Jose Bento <jldbento@gmail.com>
2024-05-06 09:27:16 -0300

    Aula POO orientada a Encapsulamento

---
+++ src/jose/luiz/terceirasemana/ComputadorPedrinho.java
@@ -0,0 +1,13 @@
+package jose.luiz.terceirasemana;
+
+public class ComputadorPedrinho {
+          public static void main(String[] args) {
+                    //abrindo MSN messenger
+                    MSNmenssager msn = new MSNmenssager();
+
+                    
+                    msn.enviarMensagem();
+
+                    msn.receberMensagem();
+          }
+}
--- src/jose/luiz/terceirasemana/MSNmenssager.java
+++ src/jose/luiz/terceirasemana/MSNmenssager.java
@@ -1,5 +1,18 @@
 package jose.luiz.terceirasemana;
 
 public class MSNmenssager {
-
+          public void enviarMensagem() {
+                    validarConectadoInternet();
+                    System.out.println("Enviando mensagem ...");
+                    salvarHistoricoMensagem();
+          }
+          public void receberMensagem() {
+                    System.out.println("Recebendo mensagem ...");
+          }
+          private void validarConectadoInternet() {
+                    System.out.println("Validando se esta conectado a internet");
+          }
+          private void salvarHistoricoMensagem() {
+                    System.out.println("Salvado o Historico....");
+          }
 }
